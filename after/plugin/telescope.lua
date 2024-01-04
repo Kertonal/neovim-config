3f4cc040ce674d0d0ab72255f819afa989b9e72d
Kertonal <>
2024-01-04 09:21:35 -0500

    Update telescope find files to include ignored files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -22,7 +22,11 @@ require('telescope').load_extension('file_browser')
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function()
+    builtin.find_files({find_command = {'rg', '--files', '--hidden', '--no-ignore-vcs', '-g', '!.git/' }})
+end)
+
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pr', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', '<leader>ps', function()
