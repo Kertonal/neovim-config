3e769a1e70b87b55d84c4a7013d98dc2787450d2
Kertonal <>
2024-01-05 09:50:05 -0500

    Update Harpoon mappings & remove tabline

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
-vim.keymap.set('n', '<leader>pa', mark.add_file)
-vim.keymap.set('n', '<leader>ph', ui.toggle_quick_menu)
+vim.keymap.set('n', '<leader>a', mark.add_file)
+vim.keymap.set('n', '<leader>e', ui.toggle_quick_menu)
 
 vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
 vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
@@ -16,5 +16,5 @@ vim.keymap.set('n', '<leader>9', function() ui.nav_file(9) end)
 vim.keymap.set('n', '<leader>0', function() ui.nav_file(0) end)
 
 require('harpoon').setup({
-    tabline = true
+    tabline = false
 })
