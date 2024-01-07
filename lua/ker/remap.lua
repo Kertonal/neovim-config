59161fd1058838d5beca836df4ab20b404916386
Kertonal <>
2024-01-07 13:36:06 -0500

    Add back remaps for visual J and K

--- lua/ker/remap.lua
+++ lua/ker/remap.lua
@@ -8,6 +8,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<C-j>", "3<C-e>")
 vim.keymap.set("n", "<C-k>", "3<C-y>")
 
+-- special way to move line in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- yanking to the clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
