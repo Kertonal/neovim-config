7327b13fcbc3923cf90a04bf9f695f4b4825329d
Kertonal <>
2024-02-25 12:12:26 -0500

    feat: disable annoying Lazy notifying

--- lua/lazy_init.lua
+++ lua/lazy_init.lua
@@ -13,4 +13,12 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    }
+})
