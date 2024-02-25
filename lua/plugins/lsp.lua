04b438c98fab7c9b894f47f31203ee441ed26f0a
Kertonal <>
2024-02-25 12:13:06 -0500

    feat: filter lsp doc symbols to funcs & methods in key mapping
    This broke the show all symbols mapping so that will need fixed at
    some point. I only plan to use this new mapping so not the biggest
    deal.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,8 +28,15 @@ return {
             nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
             nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
             nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            nmap('<leader>df', function ()
+                require('telescope.builtin').lsp_document_symbols({
+                    symbols = {
+                        'function',
+                        'method',
+                    }
+                })
+            end, '[D]ocument [F]unctions')
 
             -- See `:help K` for why this keymap
             nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
