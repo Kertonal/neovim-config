69454f99b5a49f52626e4b7047a66b36341ae031
Kertonal <>
2024-02-29 10:35:11 -0500

    feat: remove treesitter text objects
    These were making opening dart files very slow. Did not use them
    anyway.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -31,50 +31,6 @@ return {
                         node_decremental = '<M-space>',
                     },
                 },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                        keymaps = {
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                            [']]'] = '@class.outer',
-                        },
-                        goto_next_end = {
-                            [']M'] = '@function.outer',
-                            [']['] = '@class.outer',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                            ['[['] = '@class.outer',
-                        },
-                        goto_previous_end = {
-                            ['[M'] = '@function.outer',
-                            ['[]'] = '@class.outer',
-                        },
-                    },
-                    swap = {
-                        -- TODO: consider enabling this in the future
-                        enable = false,
-                        -- swap_next = {
-                        --   ['<leader>a'] = '@parameter.inner',
-                        -- },
-                        -- swap_previous = {
-                        --   ['<leader>A'] = '@parameter.inner',
-                        -- },
-                    },
-                },
             })
         end, 0)
     end
