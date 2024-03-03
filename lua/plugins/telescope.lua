34e6778b5d6c71d08449529ccd034f5025a7572f
Kertonal <>
2024-03-02 21:57:48 -0500

    feat: clean up and add telescope key maps
    Removed a bunch of commented out key maps. Enabled key map to
    fuzzy search vim docs.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -90,7 +90,6 @@ return {
 
             local builtin = require('telescope.builtin')
 
-            -- vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
             vim.keymap.set('n', '<leader>sf', function()
                 builtin.find_files({find_command = {'rg', '--files', '--hidden', '--no-ignore-vcs', '-g', '!.git/' }})
             end)
@@ -107,11 +106,10 @@ return {
                 { noremap = true }
             )
 
-            -- vim.keymap.set('n', '<leader>ss', require('telescope.builtin').builtin, { desc = '[S]earch [S]elect Telescope' })
-            -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-            -- vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-            -- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+            -- TODO: maybe enable later
+            -- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+
+            vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
             vim.keymap.set('n', '<leader>sG', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
             vim.keymap.set('n', '<leader>sg', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
             vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
