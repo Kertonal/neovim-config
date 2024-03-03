485e8a5ede36adf5d55624b9db347061bead7c09
Kertonal <>
2024-03-02 21:43:55 -0500

    feat: remove lsp format command
    Will plan to use a real formatting plugin soon so this will not be
    needed.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -53,11 +53,6 @@ return {
             nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
 
             nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-            -- Create a command `:Format` local to the LSP buffer
-            vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-                vim.lsp.buf.format()
-            end, { desc = 'Format current buffer with LSP' })
         end
 
         -- mason-lspconfig requires that these setup functions are called in this order
