e4025d48f5f2c08c36c8c0f97492797689e3ef5a
Kertonal <>
2024-03-11 09:11:23 -0400

    feat: create initial js snippets

---
+++ snippets/javascript.lua
@@ -0,0 +1,52 @@
+return {
+    s('if',
+        fmt('if ({}) {{\n\t{}\n}}', {
+            i(1),
+            i(2)
+        })
+    ),
+    s('f',
+        fmt('function {}({}) {{\n\t{}\n}}', {
+            i(1, 'Name'),
+            i(2),
+            i(3)
+        })
+    ),
+    s('try',
+        fmt('try {{\n\t{}\n}} catch ({}){{\n\t{}\n}}', {
+            i(1),
+            i(2, 'err'),
+            i(3)
+        })
+    ),
+    s('while',
+        fmt('while ({}) {{\n\t{}\n}}', {
+            i(1),
+            i(2)
+        })
+    ),
+    s('sw',
+        fmt([[
+        switch ({}) {{
+            case '{}':
+                {}
+                break;{}
+            
+            default:
+                {}
+                break;
+        }}]], {
+            i(1),
+            i(2),
+            i(3),
+            i(4),
+            i(5),
+        })
+    ),
+    s('case',
+        fmt('case \'{}\':\n\t{}\n\tbreak;', {
+            i(1),
+            i(2),
+        })
+    ),
+}
