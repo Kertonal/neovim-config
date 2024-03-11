44579f83df1d77f8e33953b1d3602fcec88d51d2
Kertonal <>
2024-03-11 09:01:20 -0400

    feat: hide mini-files icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,7 +3,12 @@ return {
         'echasnovski/mini.files',
         version = '*',
         config = function ()
-            require('mini.files').setup()
+            require('mini.files').setup({
+                -- hides icons
+                content = {
+                    prefix = function() end
+                }
+            })
 
             vim.keymap.set('n', '<leader>E', function ()
                 MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
