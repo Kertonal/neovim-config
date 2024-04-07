580b78a34f2215256fa54456e31bbb68d3b73d56
Kertonal <>
2024-04-07 13:52:35 -0400

    feat: add harpoon sync list setting

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,7 +9,8 @@ return {
         -- REQUIRED
         harpoon:setup({
             settings = {
-                save_on_toggle = true
+                save_on_toggle = true,
+                sync_on_ui_close = true,
             }
         })
         -- REQUIRED
