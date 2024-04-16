0d6539c502be44af1d57ea1e7d02ba18c8afedf9
Kertonal <>
2024-04-16 16:51:03 -0400

    feat: add keymap to view git branches in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,6 +27,12 @@ return {
                         },
                     },
                 },
+                pickers = {
+                    git_branches = {
+                        theme = 'dropdown',
+                        previewer = false
+                    }
+                }
             })
 
             -- Enable telescope fzf native, if installed
@@ -84,6 +90,7 @@ return {
             vim.keymap.set('n', '<leader>sg', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
             vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
             vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>Gb', require('telescope.builtin').git_branches)
         end
     },
 }
