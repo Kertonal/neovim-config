ba6d94e33433c7aff77bc70f58bc78fb473e0531
Kertonal <>
2025-04-09 11:55:29 -0400

    feat: re-add treesitter setting
    Might actually be needed.
    Added back comments as well.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,6 +18,10 @@ return {
                     'vimdoc',
                 },
 
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
                 auto_install = true,
 
                 highlight = {
